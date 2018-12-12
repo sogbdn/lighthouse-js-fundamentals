39ba1e0d95d522f5feae3d067da64bb33677008b
sogbdn <sonia.barretdenazaris@gmail.com>
2018-12-12 21:28:15 +0000

    Adds looping exercises

--- chorus-while.js
+++ chorus-while.js
@@ -9,8 +9,3 @@ while (repeat < 10) {
 }
 console.log("Until the sun comes up!");
 
-var chorus = "Let's dance!";
-for (var repeat = 0; repeat < 10; repeat++) {
-  console.log(chorus);
-}
-console.log("Until the sun comes up!");
