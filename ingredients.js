0212928f73ed1dc5c59e720dbb7771dac745f3a8
sogbdn <sonia.barretdenazaris@gmail.com>
2018-12-12 21:50:09 +0000

    while and for loops

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+
+var i = 0;
+while (i < 8){
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (i = 0; i < 8; i++){
+  console.log(ingredients[i]);
+}
+
+
+for (i = 7; i >= 0; i--){
+  console.log(ingredients[i]);
+}
+
+
